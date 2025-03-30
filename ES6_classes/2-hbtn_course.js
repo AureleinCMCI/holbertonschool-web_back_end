1c101e3d0ebdf36c6d08ea5173492895e947321b
Your <you@example.com>
2025-03-30 22:46:37 +0200

    proe

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,55 +1,43 @@
 class HolbertonCourse {
-    constructor(name, length, students) {
-      // Validate attribute types during object creation
-      if (typeof name !== 'string') {
-        throw new TypeError('name must be a string');
-      }
-      if (typeof length !== 'number') {
-        throw new TypeError('length must be a number');
-      }
-      if (!Array.isArray(students) || !students.every(student => typeof student === 'string')) {
-        throw new TypeError('students must be an array of strings');
-      }
-  
-      this._name = name;
-      this._length = length;
-      this._students = students;
-    }
-  
-    // Getter and Setter for 'name'
-    get name() {
-      return this._name;
-    }
-  
-    set name(value) {
-      if (typeof value !== 'string') {
-        throw new TypeError('name must be a string');
-      }
-      this._name = value;
-    }
-  
-    // Getter and Setter for 'length'
-    get length() {
-      return this._length;
-    }
-  
-    set length(value) {
-      if (typeof value !== 'number') {
-        throw new TypeError('length must be a number');
-      }
-      this._length = value;
-    }
-  
-    // Getter and Setter for 'students'
-    get students() {
-      return this._students;
-    }
-  
-    set students(value) {
-      if (!Array.isArray(value) || !value.every(student => typeof student === 'string')) {
-        throw new TypeError('students must be an array of strings');
-      }
-      this._students = value;
-    }
+  constructor(name, length, students) {
+    this.name = name;       // Utilisation des setters pour la validation
+    this.length = length;
+    this.students = students;
   }
-  
+
+  // Getter et Setter pour `name`
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== "string") {
+      throw new TypeError("name must be a string");
+    }
+    this._name = value;
+  }
+
+  // Getter et Setter pour `length`
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (typeof value !== "number") {
+      throw new TypeError("length must be a number");
+    }
+    this._length = value;
+  }
+
+  // Getter et Setter pour `students`
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    if (!Array.isArray(value) || !value.every((student) => typeof student === "string")) {
+      throw new TypeError("students must be an array of strings");
+    }
+    this._students = value;
+  }
+}
---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,6 @@
+class Currency{
+    constructor(code,name){
+        this.code
+        this.name
+    }
+}
