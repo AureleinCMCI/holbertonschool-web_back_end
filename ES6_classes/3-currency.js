40a3616077dab7c18bfa363efb3be857b27a69e0
Your <you@example.com>
2025-03-30 23:07:14 +0200

    proe

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,6 +1,36 @@
-class Currency{
+export default class Currency{
     constructor(code,name){
-        this.code
-        this.name
+        if (typeof code !== "string") {
+            throw new TypeError("code must be a string");
+          }
+          if (typeof name !== "string") {
+            throw new TypeError("name must be a string");
+          }
+        this.code = code; // Utilise le setter qui vérifie le type
+        this.name = name; 
     }
+    get name() {
+        return this._name;
+      }
+    
+    set name(value) {
+        if (typeof value !== "string") {
+          throw new TypeError("name must be a string");
+        }
+        this._name = value;
+      }
+    get code() {
+        return this._code;
+      }
+    
+    set code(value) {
+        if (typeof value !== "string") {
+          throw new TypeError("code must be a string");
+        }
+        this._code = value;
+      }
+    displayFullCurrency() {
+        return `${this._name} (${this._code})`;
+      }
 }
+
