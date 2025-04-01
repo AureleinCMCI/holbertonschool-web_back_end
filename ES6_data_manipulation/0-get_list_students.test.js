9141b98d8e3b4e1eeb6bb553659ee959743b83c4
Your <you@example.com>
2025-04-02 00:15:36 +0200

    proe

---
+++ ES6_data_manipulation/0-get_list_students.test.js
@@ -0,0 +1,9 @@
+import getListStudents from "./0-get_list_students.js";
+
+test("getListStudents doit retourner un tableau avec les bons étudiants", () => {
+  expect(getListStudents()).toEqual([
+    { id: 1, firstName: "Guillaume", location: "San Francisco" },
+    { id: 2, firstName: "James", location: "Columbia" },
+    { id: 5, firstName: "Serena", location: "San Francisco" }
+  ]);
+});
