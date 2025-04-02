db733987fa37261ffbb111a5b16ab663b086417e
Your <you@example.com>
2025-04-02 17:59:57 +0200

    proe

--- ES6_data_manipulation/0-get_list_students.test.js
+++ ES6_data_manipulation/0-get_list_students.test.js
@@ -1,12 +1,9 @@
-import getListStudents from './0-get_list_students';
+import getListStudents from './0-get_list_students.js';
 
-describe('getListStudents', () => {
-  it('doit retourner un tableau avec les bons étudiants', () => {
-    expect.assertions(1); // Indique le nombre attendu d'assertions
-    expect(getListStudents()).toStrictEqual([
-      { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-      { id: 2, firstName: 'James', location: 'Columbia' },
-      { id: 5, firstName: 'Serena', location: 'San Francisco' },
-    ]);
-  });
+test('getListStudents doit retourner un tableau avec les bons étudiants', () => {
+  expect(getListStudents()).toStrictEqual([
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ]);
 });
--- ES6_data_manipulation/1-get_list_student_ids.js
+++
@@ -1,6 +0,0 @@
-function getListStudentIds(students) {
-  if (!Array.isArray(students)) {
-    return [];
-  }
-  return students.map((student) => student.id);
-}
