d2720f6bc67fae187e81ab8def644b7cac1f28f2
Your <you@example.com>
2025-04-05 20:10:27 +0200

    proe

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,4 +1,4 @@
-function uploadPhoto(fileName) {
+export default function uploadPhoto(fileName) {
     return new Promise((_, reject) => {
       reject(new Error(`${fileName} cannot be processed`));
     });
