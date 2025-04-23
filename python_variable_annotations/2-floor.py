efb804d7916895cf78b42c0fdc3e490c8152ec84
Aurelien <aurelien78140@outlook.fr>
2025-04-23 14:47:57 +0200

    75

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+"""Module that defines a floor function with type annotations."""
+
+def floor(n: float) -> int:
+    """Return the floor (greatest integer less than or equal to n) of a float."""
+    return floor(n)
