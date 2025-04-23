44705454219947f81e83a2c4a50c8ddb8d61b557
Aurelien <aurelien78140@outlook.fr>
2025-04-23 18:35:58 +0200

    75

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -2,9 +2,9 @@
 """Module pour la fonction floor annotée."""
 
 
-from typing import List, Union , Tuple
+from typing import List, Union, Tuple
 
 
-def to_kv(k:str , v:Union[int,float] ) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Return a tuple where the first element is k and the """
-    return (k , v**2 )
+    return (k, v**2)
