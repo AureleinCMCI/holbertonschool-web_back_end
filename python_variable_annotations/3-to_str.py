a165ce231a4df1202efb2271a43e47d9328eb815
Aurelien <aurelien78140@outlook.fr>
2025-04-23 15:07:14 +0200

    75

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module pour la fonction floor annotée."""
+
+
+def to_str(n:float) -> str:
+    """Retourne le plus grand entier inférieur ou égal à n."""
+    return str(n)
