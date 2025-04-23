379686ede87fc2551c7e46ac8eb0453b698e06b9
Aurelien <aurelien78140@outlook.fr>
2025-04-23 18:51:30 +0200

    75

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -5,4 +5,6 @@ from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Return a function that multiplies its input by the given multiplier."""
+
     return [(i, len(i)) for i in lst]
