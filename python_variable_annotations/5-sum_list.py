d5b06d5497193b7f451fa50a9a5d42571b08b623
Aurelien <aurelien78140@outlook.fr>
2025-04-23 17:14:17 +0200

    75

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module pour la fonction floor annotée."""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Retourne le plus grand entier inférieur ou égal à n."""
+    return sum(input_list)
