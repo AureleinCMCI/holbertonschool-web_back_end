2dc85738aac17fccc71547d838a54c30fee25a29
Aurelien <aurelien78140@outlook.fr>
2025-04-23 14:53:30 +0200

    75

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-
 """Module pour la fonction floor annotée."""
 
-import math
 
+import math
 def floor(n: float) -> int:
     """Retourne le plus grand entier inférieur ou égal à n."""
     return math.floor(n)
