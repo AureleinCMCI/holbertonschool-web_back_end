df57932610987480a0b06217c2edcb2486ff0bef
Aurelien <aurelien78140@outlook.fr>
2025-04-23 18:33:59 +0200

    75

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,5 +3,8 @@
 
 
 from typing import List, Union , Tuple
+
+
 def to_kv(k:str , v:Union[int,float] ) -> Tuple[str, float]:
+    """Return a tuple where the first element is k and the """
     return (k , v**2 )
