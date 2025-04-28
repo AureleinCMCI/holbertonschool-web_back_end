5c0323f819dd79935e2efe1e3d6a089639eebf32
Aurelien <aurelien78140@outlook.fr>
2025-04-28 12:42:21 +0200

    75

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -6,3 +6,8 @@ import asyncio
 import time
 
 wait_n = __import__('0-basic_async_syntax.').wait_n
+
+
+def task_wait_random(max_delay: int):
+    """Crée et retourne une tâche asyncio pour wait_random(max_delay)."""
+    return asyncio.create_task(wait_n(max_delay))
