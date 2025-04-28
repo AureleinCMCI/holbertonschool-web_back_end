50dc01b39ff4a958a71340183a89c99ff2d05aa2
Aurelien <aurelien78140@outlook.fr>
2025-04-28 12:31:19 +0200

    75

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" multiple coroutines at the same time with async """
+
+from typing import List
+import asyncio
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+async def measure_time(n: int , max_delay:int ) -> float: 
+
+    """"mesure le temps d ' exécution de la fonction wain_n"""""
+
+    start = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    endtime = time.time()
+    total_time = endtime - start
+    return total_time / n
+    
