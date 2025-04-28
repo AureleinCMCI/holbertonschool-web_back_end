abbf9bb4a444e55a2961baad183e3b1a721a7875
Aurelien <aurelien78140@outlook.fr>
2025-04-28 12:35:46 +0200

    75

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -8,7 +8,7 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
 
     """"mesure le temps d ' exécution de la fonction wain_n"""""
 
---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" multiple coroutines at the same time with async """
+
+from typing import List
+import asyncio
+import time
+
+wait_n = __import__('0-basic_async_syntax.').wait_n
