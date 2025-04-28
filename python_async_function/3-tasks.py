362b1214b7f32b98a3e0866a6f6ed38c0f30b00a
Aurelien <aurelien78140@outlook.fr>
2025-04-28 12:47:52 +0200

    75

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
-""" multiple coroutines at the same time with async """
-
-from typing import List
+"""Tasks"""
 import asyncio
-import time
-
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """[summary]
 
-def task_wait_random(max_delay: int):
-    """Crée et retourne une tâche asyncio pour wait_random(max_delay)."""
+    Args:
+        max_delay (int): max delay number
+
+    Returns:
+        asyncio.Task: Creating tasks
+    """
     return asyncio.create_task(wait_random(max_delay))
