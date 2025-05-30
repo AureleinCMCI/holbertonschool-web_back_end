7121ef43f3a77f3fc2900505ca14e87619b8e75a
Your <you@example.com>
2025-05-30 13:34:16 +0200

    75

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+""" MongoDB Update """
+
+
 def schools_by_topic(mongo_collection, topic):
     """Return a list of schools having the specified topic in their documents."""
     cursor = mongo_collection.find({'topic': topic})
