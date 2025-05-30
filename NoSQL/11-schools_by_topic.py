ef30b324be5766db673c6a8ed35f45c9e777fb74
Your <you@example.com>
2025-05-30 13:33:36 +0200

    75

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,5 @@
+def schools_by_topic(mongo_collection, topic):
+    """Return a list of schools having the specified topic in their documents."""
+    cursor = mongo_collection.find({'topic': topic})
+    schools = [doc['school'] for doc in cursor if 'school' in doc]
+    return schools
