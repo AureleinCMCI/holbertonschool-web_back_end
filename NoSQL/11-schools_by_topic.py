a274422fbe3017feea310505b9e8ae51c7b88d4a
Your <you@example.com>
2025-05-30 13:38:19 +0200

    75

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
-""" MongoDB Update """
+""" 11-schools_by_topic: find schools by topic """
 
 
 def schools_by_topic(mongo_collection, topic):
-    """Return a list of schools having the specified topic in their documents."""
-    cursor = mongo_collection.find({'topic': topic})
-    schools = [doc['school'] for doc in cursor if 'school' in doc]
-    return schools
+    """
+    Return list of schools that have a specific topic.
 
+    Args:
+        mongo_collection: pymongo collection object
+        topic (str): topic to search for
+
+    Returns:
+        List of matching documents
+    """
+    return mongo_collection.find({ "topics": topic })
